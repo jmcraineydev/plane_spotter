eab1d967de236e7014044acb96ebbe05adcce23c
jmcraineydev <jmcraineydev@gmail.com>
2022-07-18 20:23:25 +0900

    feat: add migration to create planes_users_follow table

---
+++ db/migrations/20220718111058_create_planes_users_follow_table.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("planes_users_follow", function (table) {
+    table.increments("id").primary();
+    table
+      .integer("user_id")
+      .notNullable()
+      .references("id")
+      .inTable("users")
+      .onDelete("CASCADE");
+    table.string("plane_reg").notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  knex.schema.dropTable("planes_users_follow");
+};
