9d2e2dd0078c6cb755f1bbd5a866a71e78bb589b
jmcraineydev <jmcraineydev@gmail.com>
2022-07-21 19:51:04 +0900

    feat: add inital static html to server

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const userController = require("../controllers/userController");
 const setupServer = () => {
   const app = express();
   app.use(express.json());
+  app.use("/", express.static("public"));
   app.use("/api/users", userController);
 
   return app;
