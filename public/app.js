874712fb79edbee3d1415b653c3d63613fd8a35c
jmcraineydev <jmcraineydev@gmail.com>
2022-07-23 09:33:06 +0900

    feat: add query function to UI

---
+++ public/app.js
@@ -0,0 +1,34 @@
+async function submitEvent(event) {
+  event.preventDefault();
+  const userName = await document.getElementById("username").value;
+  console.log(`new un: ${userName}`);
+  if (!userName) {
+    return -1;
+  } else {
+    const flightData = await axios
+      .get(`http://localhost:4000/api/follows/${userName}`)
+      .then((response) => {
+        return response.data;
+      })
+      .catch((error) => console.log(error));
+    console.log(flightData);
+    loadResults(flightData);
+  }
+}
+
+function loadResults(data) {
+  const resultsDiv = document.querySelector("#query_results");
+
+  const flightEl = document.createElement("div");
+  flightEl.className = "flights";
+  flightEl.innerText = JSON.stringify(data, null, 4);
+
+  resultsDiv.append(flightEl);
+}
+
+function clearQuery() {
+  const query = document.getElementById("query_result");
+  const flights = document.getElementsByClassName("flights");
+  query.removeChild(flights);
+  document.querySelector("#username").reset();
+}
