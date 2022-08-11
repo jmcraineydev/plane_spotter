4693785ec22c2e4b6439696300139909d55f094b
Jason McRainey <jmcraineydev@gmail.com>
2022-08-11 15:51:06 +0900

    update: migration file

--- db/migrations/20220721105805_add_passwords_uuid_to_users_table.js
+++ db/migrations/20220721105805_add_passwords_uuid_to_users_table.js
@@ -6,16 +6,16 @@ exports.up = async function (knex) {
   await knex.schema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
   return await knex.schema.alterTable("users", function (table) {
     table.string("password", 255).notNullable();
-});
-
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = async function (knex) {
-  await knex.schema.alterTable("users", function (table) {
-    table.dropPrimary();
-    table.dropColumn("user_uid");
-    table.dropColumn("password");
   });
+
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  exports.down = async function (knex) {
+    await knex.schema.alterTable("users", function (table) {
+      table.dropPrimary();
+      table.dropColumn("password");
+    });
+  };
 };
